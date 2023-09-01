5a139bcc544deee9c52862c6e9a75533500e3a94
Arko <arkoroyb@gmail.com>
2023-09-01 11:49:57 +0600

    project setup and first prisma migrate

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,12 @@ import cors from 'cors'
 // import routes from './app/routes'
 import httpStatus from 'http-status'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import cookieParser from 'cookie-parser'
 
 const app: Application = express()
 
 app.use(cors())
-
+app.use(cookieParser())
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
