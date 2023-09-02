a3bfefd23fdf03d560ff81ca4f3ec857f07ebbd7
Arko <arkoroyb@gmail.com>
2023-09-03 00:12:27 +0600

    add dist

---
+++ dist/app/modules/user/user.constants.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userrelationalfields = exports.userSearchableFields = exports.userFilterableFields = void 0;
+exports.userFilterableFields = ['searchTerm', 'id'];
+exports.userSearchableFields = ['name', 'email'];
+exports.userrelationalfields = [''];
