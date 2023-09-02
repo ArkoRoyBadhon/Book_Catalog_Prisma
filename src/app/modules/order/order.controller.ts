e6b040ee5bc8de8cdd082758aa085c5c4856ec5a
Arko <arkoroyb@gmail.com>
2023-09-02 13:49:47 +0600

    create order complete

--- src/app/modules/order/order.controller.ts
+++ src/app/modules/order/order.controller.ts
@@ -1,21 +1,21 @@
 import { Request, Response } from 'express'
 import catchAsync from '../../../shared/catchAsync'
-// import { orderService } from './order.service'
+import { orderService } from './order.service'
 import httpStatus from 'http-status'
 import sendResponse from '../../../shared/sendResponse'
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   const token = req.headers.authorization || req.headers.Authorization
 
-  console.log('token order', token)
+  //   console.log('body', req.body.orderedBooks)
 
-  // const result = await orderService.insertIntoDB(token, req.body)
+  const result = await orderService.insertIntoDB(token, req.body.orderedBooks)
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'Order created successfully!',
-    data: 'result',
+    data: result,
   })
 })
 
